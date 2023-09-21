c4aa9974d1e512d7d86119a844486c8ddac40861
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2023-09-21 08:38:11 +0500

    Numeric Conversions Added

---
+++ NumericConversions/NumericConversions/ExplicitConversion.cs
@@ -0,0 +1,28 @@
+﻿namespace NumericConversions
+{
+    public static class ExplicitConversion
+    {
+        public static int LongToInt(long longValue)
+        {
+            return (int)longValue;
+        }
+
+        // TODO #9: Add a static method here with name "FloatToInt" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #10: Add a static method here with name "DoubleToInt" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #11: Add a static method here with name "DecimalToInt" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #12: Add a static method here with name "FloatToLong" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #13: Add a static method here with name "DoubleToLong" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #14: Add a static method here with name "DecimalToLong" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #15: Add a static method here with name "ShortToByte" that gets "shortValue" parameter ("short" type) and returns the parameter value that is explicitly converted to "byte" type.
+
+        // TODO #16: Add a static method here with name "IntToByte" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "byte" type.
+
+        // TODO #17: Add a static method here with name "IntToShort" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "short" type.
+    }
+}
